e3371461233fd4d6f2b540c9583116f470e31ebc
purbahotles <brockles1993@gmail.com>
2024-05-22 19:57:30 +0700

    Deploying to GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Projects from '../views/Projects.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Menggunakan createWebHashHistory untuk mode hash
   routes,
 })
 
