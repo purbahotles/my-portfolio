3d1a230c2ec542a85cdf3b1d984bd1f06f4951dc
purbahotles <brockles1993@gmail.com>
2024-06-18 14:28:50 +0700

    Restore files as of commit a263451f9a401a75356abb790f3c22af7d74c5d6

---
+++ src/views/Projects.vue
@@ -0,0 +1,90 @@
+<template>
+    <section class="grid sm:grid-cols-1 lg:grid-cols-2">
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">Tiger</h1>
+        <div class="relative max-w-300 mt-5">
+          <img src="../assets/img/tiger-roar.jpg" alt="Banner Image" class="w-full h-auto opacity-60">
+            <div class="text-primary-dark mt-5">
+              <p>Project Name : Tiger Roar</p>
+              <p>Tech : Html, Css, Javascript, PHP, SCSS, Vuejs, Laravel, Bootstrap, Wabpack, Bitbucket, mysql.</p>
+              <p>Role : FullStack Developer</p>
+              <p>Other : Ubuntu, MVC, SSR, Rest Api, Apache</p>
+            </div>
+        </div>
+      </section>
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">Raffles</h1>
+        <div class="relative max-w-300 mt-5">
+          <img src="../assets/img/raflesmooncake.jpg" alt="Banner Image" class="w-full h-auto opacity-60">
+            <div class="text-primary-dark mt-5">
+              <p>Project Name : Raffles Mooncake</p>
+              <p>Tech : Html, Css, Javascript, PHP, SCSS, Vuejs, Laravel, Bootstrap, Wabpack, Bitbucket, mysql.</p>
+              <p>Role : FullStack Developer</p>
+              <p>Other : Ubuntu, MVC, CMS, SSR, Rest Api, Apache</p>
+            </div>
+        </div>
+      </section>
+
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">SLH</h1>
+        <div class="relative max-w-300 mt-5">
+          <img src="../assets/img/slh-registry.jpg" alt="Banner Image" class="w-full h-auto opacity-60">
+            <div class="text-primary-dark mt-5">
+              <p>Project Name : SLH Registry (Small Luxury Hotels Of The World)</p>
+              <p>Tech : Html, Css, Javascript, PHP, SCSS, Vuejs, Laravel, Bootstrap, Wabpack, Bitbucket, mysql.</p>
+              <p>Role : FullStack Developer</p>
+              <p>Other : Ubuntu, MVC, SSR, Rest Api, Apache</p>
+            </div>
+        </div>
+      </section>
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">GUCCI</h1>
+        <div class="relative max-w-400 mt-5">
+          <img src="../assets/img/gucci-beauty-singapore.jpg" alt="Banner Image" class="w-full h-auto opacity-60">
+            <div class="text-primary-dark mt-5">
+              <p>Project Name : Gucci Beauty Singapore And Malaysia</p>
+              <p>Tech : Html, Css, Javascript, PHP, SCSS, Angularjs, Laravel, Bootstrap, Wabpack, Bitbucket, Mysql.</p>
+              <p>Role : FullStack Developer</p>
+              <p>Other : Ubuntu, MVC, SSR, Rest Api, Apache, E-commerce</p>
+            </div>
+        </div>
+      </section>
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">Shiseido</h1>
+        <div class="relative flex flex-col items-center justify-center mt-5">
+          <img src="../assets/img/shiseido.png" alt="Banner Image" class="w-full max-w-64 max-h-40rem h-auto opacity-60">
+          <div class="text-primary-dark mt-5 text-left">
+            <p>Project Name: Shiseido Camellia Club</p>
+            <p>Tech: Html, Css, Javascript, PHP, SCSS, Vuejs, Laravel, Bootstrap, Webpack, Sql Server.</p>
+            <p>Role: FullStack Developer</p>
+            <p>Other: Ubuntu, MVC, SSR, Rest Api, Apache, E-commerce</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="bg-primary-light p-8 m-5 border border-primary-light">
+        <h1 class="text-4xl text-center font-bold text-primary-dark">Prime</h1>
+        <div class="relative max-w-400 mt-5">
+          <img src="../assets/img/los-prime.jpg" alt="Banner Image" class="w-full h-auto opacity-60">
+            <div class="text-primary-dark mt-5">
+              <p>Project Name : Los Prime (Loan Origin System)</p>
+              <p>Tech : Html, Css, Typescript, Vuetify, Vuejs, Vuex, Vue Router, Webpack, Github.</p>
+              <p>Role : Frontend Developer</p>
+              <p>Other : Ubuntu, PWA, SPA, Micro Service, CI/CD Pipelines, Nginx, Kubernetes, Docker, PostgreSql</p>
+            </div>
+        </div>
+      </section>
+
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+}
+</script>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
+
+export default defineConfig({
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
