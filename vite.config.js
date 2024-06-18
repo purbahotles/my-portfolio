db6fdbed9d66669753ab5c01a6ce5691476cbeca
purbahotles <brockles1993@gmail.com>
2024-06-18 11:53:15 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
   plugins: [vue()],
   base: '/my-portfolio/',
-  // base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
   css: {
     postcss: {
       plugins: [tailwindcss],
